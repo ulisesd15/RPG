41fe6f8ac3fe426ad469ca0391fca03413bbd936
ulisesd404 <144632800+ulisesd404@users.noreply.github.com>
2023-10-22 11:01:12 -0700

    steps have been set up ready to code

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); 
 
+function generatePassword() {
+//password criteria
+
+//length of password is 8-128 characters
+    
+//lowecase
+
+//uppercase
+
+//numbers
+
+//special chcaracters
+
+//ensures all requirements
+
+//generate password
+
+//diplays passwords
+    return
+}
 
 // Write password to the #password input
 function writePassword() {
