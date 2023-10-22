28cc4a80b078c581e59ea782f60e1eca9f91c141
ulisesd404 <144632800+ulisesd404@users.noreply.github.com>
2023-10-22 14:26:12 -0700

    set up all requirements, no randomization yet

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); 
 
+var numericValues = "0123456789".split("");
+var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz".split("");
+var specialCharacters = "!#$%&')(*+,-./:;<=>?@][\^_`}{|~".split("");
+
 function generatePassword() {
+  var includeLowerCaseLetters = false
+  var includeUpperCaseLetters = false
+  var includeNumericValues = false
+  var includeSpecialCharacters= false
+
 //password criteria
 
 //length of password is 8-128 characters
-    
+var numberOfCharacters = prompt("How long should your password be? Chose between 8 and 128 characters.")
+numberOfCharacters = parseInt
+
+
+if (isNaN(numberOfCharacters) || numberOfCharacters<8 || numberOfCharacters>128) {
+  return "Please choose a password length between 8 and 128"
+}
+
+
+else {
+  alert ("Your password will be " + numberOfCharacters + " characters long.")
+}
 //lowecase
-
+includeLowerCaseLetters = confirm ("Would you like lower case letters in your password?")
 //uppercase
-
+includeUpperCaseLetters = confirm ("Would you like upper case letters in your password?")
 //numbers
-
+includeNumericValues = confirm ("Would you like to include numeric values in you password?")
 //special chcaracters
-
+includeSpecialCharacters = confirm ("Would you like special characters in your passwords?")
 //ensures all requirements
 
 //generate password
 
 //diplays passwords
-    return
+    return;
 }
 
 // Write password to the #password input
